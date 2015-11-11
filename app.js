3735677406d4af43d354c73c832853c252d6850a
dennisborba <dennis.borba@hotmail.com>
2015-11-11 00:07:56 +0000

    adicionado MVC com  angular

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require("express");
-var app = express();
+var routes = require("./routes");
+var ejs = require('ejs');
+var path = require('path');
 
-app.get('/', function(req, res) {
-    res.send("Hello world!");
-});
+var app = express();
+app.get('/', routes.index);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen( process.env.VCAP_APP_PORT || 8080 );
